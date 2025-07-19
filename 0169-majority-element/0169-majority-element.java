b695bdaa02f9d1798505fd789f0deb3e903411aa
Janani20004 <janajanani024@gmail.com>
2025-07-19 15:48:57 +0530

    Time: 2 ms (73.66%), Space: 53.1 MB (59.65%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count =0;
+        int max =0;
+        for(int num:nums ){
+            if(count==0){
+                max=num;
+            }
+            count+=(num==max)?1:-1;
+        }
+        return max;
+
+        
+    }
+}
